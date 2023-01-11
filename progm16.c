1befe2a7a2b5e12366a36d839b52912051c0587e
balaji <113337367+kattabalaji@users.noreply.github.com>
2023-01-11 13:45:59 +0530

    Update progm16.c

--- progm16.c
+++ progm16.c
@@ -4,18 +4,17 @@
       int c;
       fp = fopen("file.txt","w+");
       fputs("This is study.com", fp);
-      // We are using fseek() to shift the file pointer to the 7th position.
+     
       fseek( fp, 7, SEEK_SET );
      
-    //Now we overwrite C programming in the 7th position
+   
       fputs(" C Programming", fp);
      
-    //Now we print the current position of the file pointer using ftell()
+   
       printf("The current position of the file pointer is: %ld\n", ftell(fp));
-    //We take the file pointer to the beginning of the file.
+   
       rewind(fp);
-     
-    //Now we verify if rewind() worked using ftell().
+    
       printf("The current position of the file pointer is: %ld\n", ftell(fp));
       while(1) { 
        c = fgetc(fp);
