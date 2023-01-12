9ebbf2b9fe76ca380c34e187db31509ec3f6b02e
balaji <113337367+kattabalaji@users.noreply.github.com>
2023-01-12 10:44:01 +0530

    Update progm21.c

--- progm21.c
+++ progm21.c
@@ -1,19 +1,13 @@
-// C Program for Worst Fit
 #include <stdio.h>
 
 void implimentWorstFit(int blockSize[], int blocks, int processSize[], int processes)
 {
-    // This will store the block id of the allocated block to a process
     int allocation[processes];
-    
-    // initially assigning -1 to all allocation indexes
-    // means nothing is allocated currently
+
     for(int i = 0; i < processes; i++){
         allocation[i] = -1;
     }
- 
-    // pick each process and find suitable blocks
-    // according to its size ad assign to it
+
     for (int i=0; i<processes; i++)
     {
         
@@ -22,24 +16,18 @@ void implimentWorstFit(int blockSize[], int blocks, int processSize[], int proce
         { 
             if (blockSize[j] >= processSize[i])
             {
-                // place it at the first block fit to accomodate process
                 if (indexPlaced == -1)
                     indexPlaced = j;
-                    
-                // if any future block is larger than the current block where
-                // process is placed, change the block and thus indexPlaced
+
                 else if (blockSize[indexPlaced] < blockSize[j])
                     indexPlaced = j;
             }
         }
- 
-        // If we were successfully able to find block for the process
+
         if (indexPlaced != -1)
         {
-            // allocate this block j to process p[i]
             allocation[i] = indexPlaced;
  
-            // Reduce available memory for the block
             blockSize[indexPlaced] -= processSize[i];
         }
     }
@@ -55,7 +43,6 @@ void implimentWorstFit(int blockSize[], int blocks, int processSize[], int proce
     }
 }
  
-// Driver code
 int main()
 {
     int blockSize[] = {5, 4, 3, 6, 7};
